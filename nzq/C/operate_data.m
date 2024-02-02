aa0d6fe492d07f2d0841c8d3ba9f5b3c38834693
XXAlexMasonXX <1727411859@qq.com>
2024-02-02 20:21:55 +0800

    find 5 runs

--- nzq/C/operate_data.m
+++ nzq/C/operate_data.m
@@ -23,15 +23,85 @@ for i = 1:set_number
 end
 %% find runs of success,RoS,连续得分
 runs = 5;%number of runs considered as RoS
-%for p1
+%for p1,p1连续得分分数=p2得分不变的次数
 p1_RoS_num = zeros(set_number,1);%number of RoS in ith set
-p1_RoS_time = zeros(set_number,floor(max(set_cap)/runs));
+p1_RoS_time = zeros(set_number,floor(max(set_cap)/runs));%index of RoS in each set
 for i = 1:set_number
     %RoS in ith set
-    count = 1;
+    RoS_count = 0;%number of RoS in a set
+    continual_count = 0;
+    value = -1;
     for j = set_index(i):set_index(i+1)-1
-        %p2_points_won
+        %judge p2_points_won(j)
+        if p2_points_won(j)~=value
+            %encounter new point
+            value = p2_points_won(j);
+            if continual_count>=5
+                % valid RoS
+                RoS_count = RoS_count + 1;
+                p1_RoS_time(i,RoS_count) = j-runs;
+                continual_count = 1;
+                continue
+            else
+                % invalid RoS
+                continual_count = 1;
+                continue
+            end
+        else
+            %same point
+            continual_count = continual_count + 1;
+        end
+    end
+    p1_RoS_num(i) = RoS_count;
+end
+% %print first 5runs in each set
+% for i = 1:set_number
+%     fprintf('set No.%d: ',i);
+%     for j = 1:5
+%         fprintf('%d ',p1_points_won(p1_RoS_time(i,1)+j-1));
+%     end
+%     fprintf('\n');
+% end
 
+%for p2,p2连续得分分数=p1得分不变的次数
+p2_RoS_num = zeros(set_number,1);%number of RoS in ith set
+p2_RoS_time = zeros(set_number,floor(max(set_cap)/runs));%index of RoS in each set
+for i = 1:set_number
+    %RoS in ith set
+    RoS_count = 0;%number of RoS in a set
+    continual_count = 0;
+    value = -1;
+    for j = set_index(i):set_index(i+1)-1
+        %judge p2_points_won(j)
+        if p1_points_won(j)~=value
+            %encounter new point
+            value = p1_points_won(j);
+            if continual_count>=5
+                % valid RoS
+                RoS_count = RoS_count + 1;
+                p2_RoS_time(i,RoS_count) = j-runs;
+                continual_count = 1;
+                continue
+            else
+                % invalid RoS
+                continual_count = 1;
+                continue
+            end
+        else
+            %same point
+            continual_count = continual_count + 1;
+        end
+    end
+    p2_RoS_num(i) = RoS_count;
+end
+% %print first 5runs in each set
+% for i = 1:set_number
+%     fprintf('set No.%d: ',i);
+%     for j = 1:5
+%         fprintf('%d ',p2_points_won(p2_RoS_time(i,1)+j-1));
+%     end
+%     fprintf('\n');
+% end
 
 %% output ith set image
 % output first 8 sets
